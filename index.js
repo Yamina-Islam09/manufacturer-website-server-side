48f8bc3ba3347a474c24c003b9c41981ded89c55
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-29 17:21:59 +0600

    all orders portion

--- index.js
+++ index.js
@@ -157,7 +157,12 @@ async function run() {
         res.send(booking);
       })
   
-  
+      app.delete('/booking/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const result = await bookingCollection.deleteOne(filter);
+        res.send(result);
+      });
       app.post('/booking', async (req, res) => {
         const booking = req.body;
         
